bb949dc896745cae281a9c001769c80fb376172d
AnzhelaTarabrina <88933121+AnzhelaTarabrina@users.noreply.github.com>
2023-12-28 20:19:32 +0300

    Правки

--- js/script.js
+++ js/script.js
@@ -1,3 +1,131 @@
+// Валидация
+const validateForms = (selector, rules, afterSend) => {
+    const form = document?.querySelector(selector);
+    const telSelector = form?.querySelector('input[type="tel"]');
+
+    if (!form) {
+        console.error('Нет такого селектора!');
+        return false;
+    }
+
+    if (!rules) {
+        console.error('Вы не передали правила валидации!');
+        return false;
+    }
+
+    if (telSelector) {
+        const inputMask = new Inputmask('+7 (999) 999-99-99');
+        inputMask.mask(telSelector);
+
+        for (let item of rules) {
+        if (item.tel) {
+            item.rules.push({
+            rule: 'function',
+            validator: function() {
+                const phone = telSelector.inputmask.unmaskedvalue();
+                return phone.length === 10;
+            },
+            errorMessage: item.telError
+            });
+        }
+        }
+    }
+
+    const validation = new JustValidate(selector);
+
+    for (let item of rules) {
+        validation
+        .addField(item.ruleSelector, item.rules);
+    }
+
+    validation.onSuccess((ev) => {
+        let formData = new FormData(ev.target);
+
+        let xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+            if (afterSend) {
+                afterSend();
+            }
+            console.log('Отправлено');
+            }
+        }
+        }
+
+        xhr.open('POST', 'mail.php', true);
+        xhr.send(formData);
+
+        ev.target.reset();
+    })
+};
+
+const rules = [
+    {
+        ruleSelector: '.input-name',
+        rules: [
+        {
+            rule: 'minLength',
+            value: 3
+        },
+        {
+            rule: 'required',
+            value: true
+        }
+        ]
+    },
+    {
+        ruleSelector: '.input-email',
+        rules: [
+        {
+            rule: 'email',
+            value: true,
+            errorMessage: 'Введите корректтные email!'
+        },
+        {
+            rule: 'required',
+            value: true
+        }
+        ]
+    },
+    {
+        ruleSelector: '.input-tel',
+        tel: true,
+        telError: 'Введите корректный телефон',
+        rules: [
+            {
+                rule: 'required',
+                value: true
+            }
+        ]
+    },
+    {
+        ruleSelector: '.input-checkbox',
+        rules: [
+        {
+            rule: 'required',
+            value: true
+        }
+        ]
+    },
+    ];
+
+    const afterForm = () => {
+        console.log('Произошла отправка');
+        sendAnimation();
+    };
+    
+    validateForms('.contact-us__form', rules, afterForm);
+
+
+// Анимация отправки сообщения
+const sendAnimation = () => {
+    document.querySelector('.form__btn-message').classList.add("form__btn-message--active");
+    document.querySelector('.animation-form').classList.add("animation-form--active");
+}
+
+
 // Меню бургер
 const iconMenu = document.querySelector(".header__burger");
 const menuBody = document.querySelector(".header__menu");
@@ -67,4 +195,5 @@ const swiper = new Swiper('.slider__swiper', {
     },
     slidesPerView: 1,
     spaceBetween: 20,
+    loop: true,
     });
